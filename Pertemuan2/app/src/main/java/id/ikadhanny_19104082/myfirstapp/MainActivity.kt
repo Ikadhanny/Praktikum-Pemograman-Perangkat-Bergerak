4119f0a060e193f86d6b7605828642c7c2eb42a3
Ikadhanny Yudyan Pratama <19104082@ittelkom-pwt.ac.id>
2021-11-03 22:53:32 +0700

    Update Pertemuan 2

---
+++ Pertemuan2/app/src/main/java/id/ikadhanny_19104082/myfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ikadhanny_19104082.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
